958a6bb40f72b53d21f090d896d965c0fd1b60e1
key90 <91842317+key90@users.noreply.github.com>
2024-12-16 05:58:22 +0300

    Update sendTelegramMessage.js

--- api/sendTelegramMessage.js
+++ api/sendTelegramMessage.js
@@ -1,27 +1,26 @@
-const fetch = require('node-fetch'); // Для отправки запросов в Telegram API
+const fetch = require('node-fetch');
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
 async function sendTelegramMessage(message) {
-  const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN; // Токен из .env
-  const CHAT_ID = process.env.CHAT_ID; // ID чата из .env
-
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
+
   const body = {
     chat_id: CHAT_ID,
     text: message,
-    parse_mode: 'HTML'
+    parse_mode: 'HTML',
   };
 
   const response = await fetch(url, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body)
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
   });
 
-  if (!response.ok) {
-    const errorDetails = await response.text();
-    throw new Error(`Ошибка отправки сообщения в Telegram: ${errorDetails}`);
+  const data = await response.json();
+  if (!response.ok || !data.ok) {
+    throw new Error(data.description || 'Ошибка отправки сообщения');
   }
 }
 
